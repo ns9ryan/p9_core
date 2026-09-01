07c096aa131d79854df433b9b930f23c254a3e57
ryan <ryan@company.com>
2026-09-01 15:30:12 +0800

    fix: 修改README.md

--- rpc/ent/schema/department.go
+++ rpc/ent/schema/department.go
@@ -4,11 +4,12 @@ import (
 	"entgo.io/ent"
 	"entgo.io/ent/dialect/entsql"
 	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
 	"github.com/ns9ryan/common/orm/ent/mixins"
 )
 
-// Department holds the schema definition for the Department entity.
+// Department 定义部门表结构
 type Department struct {
 	ent.Schema
 }
@@ -17,27 +18,51 @@ type Department struct {
 func (Department) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("name").
-			Comment("Department name | 部门名称"),
-		field.String("ancestors").Optional().
-			Comment("Parents' IDs | 父级列表"),
+			Comment("部门名称"),
+
+		field.Uint64("parent_id").
+			Optional().
+			Default(0).
+			Comment("父级部门 ID"),
+
+		field.String("ancestors").
+			Optional().
+			Comment("父级部门列表"),
+
 		field.String("leader").
-			Comment("Department leader | 部门负责人").Optional(),
+			Optional().
+			Comment("部门负责人"),
+
 		field.String("phone").
-			Comment("Leader's phone number | 负责人电话").Optional(),
+			Optional().
+			Comment("负责人电话"),
+
 		field.String("email").
-			Comment("Leader's email | 部门负责人电子邮箱").Optional(),
-		field.String("remark").Optional().
-			Comment("Remark | 备注"),
-		field.Uint64("parent_id").Optional().Default(0).
-			Comment("Parent department ID | 父级部门ID"),
+			Optional().
+			Comment("负责人邮箱"),
+
+		field.String("remark").
+			Optional().
+			Comment("备注"),
 	}
 }
 
-// Edges of the Department.
+// Edges 定义部门表关联关系
 func (Department) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		// 部门上下级关系
+		edge.To("children", Department.Type).
+			From("parent").
+			Unique().
+			Field("parent_id"),
+
+		// 部门与用户关系
+		edge.From("users", User.Type).
+			Ref("departments"),
+	}
 }
 
+// Mixin 定义部门表公共字段
 func (Department) Mixin() []ent.Mixin {
 	return []ent.Mixin{
 		mixins.IDMixin{},
@@ -46,10 +71,11 @@ func (Department) Mixin() []ent.Mixin {
 	}
 }
 
+// Annotations 定义部门表数据库注解
 func (Department) Annotations() []schema.Annotation {
 	return []schema.Annotation{
-		entsql.WithComments(true),
-		schema.Comment("Department Table | 部门表"),
-		entsql.Annotation{Table: "sys_departments"},
+		entsql.WithComments(true),                   // 启用数据库字段注释
+		schema.Comment("部门表"),                       // 设置数据库表注释
+		entsql.Annotation{Table: "sys_departments"}, // 设置数据库表名
 	}
 }

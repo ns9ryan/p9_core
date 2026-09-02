27658067140749a78e9dafab3e9113c26f7bfbea
ryan <ryan@company.com>
2026-09-02 09:59:46 +0800

    1

--- pkg/grpcerror/status.go
+++ pkg/grpcerror/status.go
@@ -1,4 +1,4 @@
-package rpcerror
+package grpcerror
 
 import (
 	"google.golang.org/grpc/codes"
--- rpc/internal/dberror/ent.go
+++ rpc/internal/dberror/ent.go
@@ -1,7 +1,7 @@
 package dberror
 
 import (
-	"github.com/ns9ryan/p9_core/pkg/rpcerror"
+	"github.com/ns9ryan/p9_core/pkg/grpcerror"
 	"github.com/ns9ryan/p9_core/rpc/ent"
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -11,22 +11,22 @@ func HandleEnt(logger logx.Logger, err error) error {
 	switch {
 	case ent.IsNotFound(err):
 		logger.Errorw("数据不存在", logx.Field("error", err.Error()))
-		return rpcerror.NotFound("目标不存在")
+		return grpcerror.NotFound("目标不存在")
 
 	case ent.IsConstraintError(err):
 		logger.Errorw("数据约束错误", logx.Field("error", err.Error()))
-		return rpcerror.InvalidArgument("数据约束错误")
+		return grpcerror.InvalidArgument("数据约束错误")
 
 	case ent.IsValidationError(err):
 		logger.Errorw("数据校验错误", logx.Field("error", err.Error()))
-		return rpcerror.InvalidArgument("数据校验错误")
+		return grpcerror.InvalidArgument("数据校验错误")
 
 	case ent.IsNotSingular(err):
 		logger.Errorw("查询结果不唯一", logx.Field("error", err.Error()))
-		return rpcerror.Internal("数据异常")
+		return grpcerror.Internal("数据异常")
 
 	default:
 		logger.Errorw("数据库操作失败", logx.Field("error", err.Error()))
-		return rpcerror.Internal("数据库操作失败")
+		return grpcerror.Internal("数据库操作失败")
 	}
 }
